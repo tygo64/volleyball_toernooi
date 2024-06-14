7b640711cfa22e6d837f33c2e0f77429e886c58e
Tygo <altijdgaming012@gmail.com>
2024-06-14 11:26:27 +0200

    test commit 2

--- volleybal-Toernooi/volleybal toernooi/includes/header.php
+++ volleybal-Toernooi/volleybal toernooi/includes/header.php
@@ -11,7 +11,7 @@
           <li><a href="index.php?PaginaNr=3">Wedstrijdschema</a></li>
           <li><a href="index.php?PaginaNr=4">Teams</a></li>
           <li><a href="index.php?PaginaNr=5">Inschrijven</a></li>
-          <li><a href="index.php?PaginaNr=6"> Admin</a></li>
+          <li><a href="index.php?PaginaNr=6">Admin</a></li>
             </ul>
         </nav>
 
