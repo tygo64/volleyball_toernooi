80518a9190903602b0ed50c85973d96aa85a9ddb
Terrific <terrific1337@gmail.com>
2024-06-19 12:23:31 +0200

    finished inschrijven

---
+++ volleybal-Toernooi/volleybal toernooi/includes/DBconnection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // Replace with your server name or IP address
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$dbname = "volleybal_club"; // Replace with your database name
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
