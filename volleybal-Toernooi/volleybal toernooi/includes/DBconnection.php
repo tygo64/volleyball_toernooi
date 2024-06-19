3dc9432611047f6d2630acbbe315e733d45f25d0
Tygo <altijdgaming012@gmail.com>
2024-06-19 12:30:34 +0200

    nieuwe data connectie

--- volleybal-Toernooi/volleybal toernooi/includes/DBconnection.php
+++ volleybal-Toernooi/volleybal toernooi/includes/DBconnection.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< inschrijven-anilcan
 $servername = "localhost"; // Replace with your server name or IP address
 $username = "root"; // Replace with your database username
 $password = ""; // Replace with your database password
@@ -13,17 +12,3 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
-=======
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=volleybal_club", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
-  ?>
->>>>>>> main
