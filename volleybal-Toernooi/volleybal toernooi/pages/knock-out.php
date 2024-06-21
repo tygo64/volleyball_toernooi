1ac0276c659cf7fbf6fa2436d11f409bdb991183
Tygo <altijdgaming012@gmail.com>
2024-06-21 08:51:56 +0200

    Knockout update

--- volleybal-Toernooi/volleybal toernooi/pages/knock-out.php
+++ volleybal-Toernooi/volleybal toernooi/pages/knock-out.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheets/knockout.css">
+    <link rel="stylesheet" href="stylesheets/knockouts.css">
     <title>Tournament Bracket</title>
 </head>
 <body>
